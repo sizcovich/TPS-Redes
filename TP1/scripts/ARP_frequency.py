bfc4927710e4aaecec64187c3309b76d6a82bdde
marcos_rogani <mrogani@users.noreply.github.com>
2014-09-20 02:02:19 +0000

    graficos de barra - cantidad de muestras - entropia

--- TP1/scripts/ARP_frequency.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-from scapy.all import *
-def monitor_callback(pkt):
-	if ARP in pkt:
-		if pkt[ARP].op == 1:
-			print pkt[ARP].psrc + " " + pkt[ARP].pdst
-
-if __name__ == '__main__':
-	sniff(prn=monitor_callback, filter = "arp", store = 0)
--- TP1/scripts/information_calculator.py
+++ TP1/scripts/information_calculator.py
@@ -1,23 +1,23 @@
 #! /usr/bin/env python
-import sys, math
+import sys, math, operator
 
 if __name__ == '__main__':
 	file_aux = open(sys.argv[1], 'r')
 	dicc_ip_rep = {}
+	dicc_ip_info = {}
 	length = 0
-
 	for line in file_aux:
 		if(line in dicc_ip_rep):
 			dicc_ip_rep[line] = dicc_ip_rep[line] + 1 
 		else:
 			dicc_ip_rep[line] = 1
 		length = length + 1
-	entropy = 0
 	for key in dicc_ip_rep.keys():
 		prob = dicc_ip_rep[key]*1.0/length
-		print "prob" + str(prob )
 		info  = math.log(1/prob,2)
-		print "info" + str(info)
-		entropy = entropy + prob*info
-	print entropy
-	print length
+		dicc_ip_info[key] = info		
+		#print key.rstrip() + " " + str(info)
+	sorted_ip_info = sorted(dicc_ip_info.iteritems(), key=operator.itemgetter(1))
+	for [ip , info] in sorted_ip_info:
+		print ip.rstrip() + " " + str(info)
+	
