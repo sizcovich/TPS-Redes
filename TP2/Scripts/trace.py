5c7f8e3a9c022038c54fb9dc7c5e3bdd5c456d20
marcos_rogani <mrogani@users.noreply.github.com>
2014-10-13 01:27:18 +0000

    workaround para cuando trace, cuando se pierden paquetes

--- TP2/Scripts/trace.py
+++ TP2/Scripts/trace.py
@@ -26,10 +26,10 @@ if __name__ == '__main__':
 				ip_list.append(icmp_pck.src)
 				#print "time: " + str(delta)				
 				print str(ttl_aux) + " " + str(icmp_pck.src) + " " + str(delta) 
-				iplong = int(ipaddress.IPv4Address(unicode('201.212.5.12'))) #int(ipaddress.ip_address('201.212.5.12'))
+				iplong = int(ipaddress.ipaddress(unicode('201.212.5.12'))) #int(ipaddress.ip_address('201.212.5.12'))
 				print iplong
 				print gic.record_by_addr(str(iplong))
-				print gic.record_by_addr('3386115340')
+				#print gic.record_by_addr('3386115340')
 				
 			else:
 				print str(ttl_aux) + " " + "*"	
@@ -42,7 +42,9 @@ if __name__ == '__main__':
 		while(i < 3):
 			packet = IP(dst=ip) / ICMP()
 			#packet.display()
-			ans,unans = sr(packet, timeout = 1, verbose = 0)
+			ans,unans = sr(packet, timeout = 10, verbose = 0)
+			while(len(ans[ICMP]) == 0 ):
+				ans,unans = sr(packet, timeout = 10, verbose = 0)
 			icmp_pck = ans[ICMP][0][1]								
 			tx_time = ans[0][0].sent_time
 			rx_time = ans[0][1].time
