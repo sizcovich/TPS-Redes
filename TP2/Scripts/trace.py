2e99b9604dcb48ce96e87be4ec1589f7473bc389
marcos_rogani <mrogani@users.noreply.github.com>
2014-10-13 02:15:40 +0000

    trace

--- TP2/Scripts/trace.py
+++ TP2/Scripts/trace.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
 	ttl_max = int(sys.argv[2])
 	ip_list = []
 	gic = pygeoip.GeoIP('GeoLiteCity.dat')
+	ip_rtt_dicc = {}
 	while(ttl_aux < ttl_max):
 		packet = IP(dst=sys.argv[1], ttl=ttl_aux) / ICMP()
 		time.sleep(0.5)
@@ -26,9 +27,10 @@ if __name__ == '__main__':
 				rx_time = ans[0][1].time
 				delta = (rx_time - tx_time)*1000
 				ip_list.append(icmp_pck.src)
+				ip_rtt_dicc[icmp_pck.src] = [delta]
 				#print "time: " + str(delta)				
 				print str(ttl_aux) + " " + str(icmp_pck.src) + " " + str(delta) 
-				iplong = int(ipaddress.IPv4Address(unicode('201.212.5.12'))) #int(ipaddress.ip_address('201.212.5.12'))
+				iplong = int(ipaddress.IPv4Address(unicode(str(icmp_pck.src)))) #int(ipaddress.ip_address('201.212.5.12'))
 				print iplong
 				print gic.record_by_addr(str(iplong))
 				#print gic.record_by_addr('3386115340')
@@ -46,14 +48,14 @@ if __name__ == '__main__':
 			#packet.display()
 			time.sleep(0.5)
 			ans,unans = sr(packet, timeout = 10, verbose = 0)
-			while(len(ans[ICMP]) == 0 ):
-				time.sleep(0.5)
-				ans,unans = sr(packet, timeout = 10, verbose = 0)
-			icmp_pck = ans[ICMP][0][1]								
-			tx_time = ans[0][0].sent_time
-			rx_time = ans[0][1].time
-			delta = (rx_time - tx_time)*1000
-			time_list.append(delta)		
+			if(len(ans[ICMP]) != 0 ):
+				icmp_pck = ans[ICMP][0][1]								
+				tx_time = ans[0][0].sent_time
+				rx_time = ans[0][1].time
+				delta = (rx_time - tx_time)*1000
+				ip_rtt_dicc[ip].append(delta)
+				time_list.append(delta)		
 			i = i+1
-		ip_dicc[ip] = [numpy.mean(time_list), numpy.std(time_list)]
+		ip_dicc[ip] = [numpy.mean(ip_rtt_dicc[ip]), numpy.std(ip_rtt_dicc[ip])]
+	print ip_rtt_dicc
 	print ip_dicc
