66fe0a250c4d45edbf440d05f0170fea940b7249
Sabrina <sizcovich@gmail.com>
2015-04-29 00:17:47 -0300

    elimino cursada vieja

--- cursada vieja/TP-2/Columbus-III/enlace_columbus3_trace.bat
+++
@@ -1,7 +0,0 @@
-tracert www.cm-lisboa.pt > enlaces_portugal.txt
-tracert www.valorsul.pt >> enlaces_portugal.txt
-tracert www.lisboaenova.org >> enlaces_portugal.txt
-tracert www.lispolis.pt >> enlaces_portugal.txt
-tracert www.marl.pt >> enlaces_portugal.txt
-tracert www.visitlisboa.com >> enlaces_portugal.txt
-tracert www.simtejo.pt >> enlaces_portugal.txt
--- cursada vieja/TP-2/TAT14/enlace_tat14_trace.bat
+++
@@ -1,5 +0,0 @@
-tracert www.nuukkunstmuseum.gl > enlaces_groenlandia.txt
-tracert www.groenlandia.com >> enlaces_groenlandia.txt
-tracert www.ilisimatusarfik.gl >> enlaces_groenlandia.txt
-tracert www.natmus.gl >> enlaces_groenlandia.txt
-tracert www.arktiskcenter.gl >> enlaces_groenlandia.txt
